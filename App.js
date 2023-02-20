6584b1349abfdaee0b758b3a403a72f5587c5b80
eselethegoat <123470747+eselethegoat@users.noreply.github.com>
2023-02-20 13:53:12 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+import LoginForm from './components/loginform';
+import './App.css'
+
+function App() {
+  return (
+    <div className="page">
+     <LoginForm />
+       
+    
+    </div>
+  );
+}
+
+
+export default App;
+ 
